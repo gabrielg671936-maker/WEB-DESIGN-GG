0532fe37601284aafbc514b4700e8b72f4f95a45
gabrielg671936-maker <gabrielg671936@gmail.com>
2026-03-24 13:57:54 -0400

    Implement typewriter effect for title and paragraph
    Added typewriter effect for title and paragraph text.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+// HOME
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -7,3 +11,35 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+
+  const text = 'GABRIEL GALICIA';
+let i = 0;
+
+function typeWriterTitle() {
+  if (i < text.length) {
+    document.getElementById("typewriter-h1").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriterTitle, 90);
+  }
+}
+
+typeWriterTitle();
+
+const textElement = document.getElementById("typewriter-text");
+const textToType = "WEB DESIGN PORTFOLIO";
+const speed = 99;
+
+let j = 0;
+
+function typeWriterParagraph() {
+  if (j < textToType.length) {
+    textElement.innerHTML += textToType.charAt(j);
+    j++;
+    setTimeout(typeWriterParagraph, speed);
+  }
+}
+
+// Delay paragraph until title finishes
+const titleDuration = text.length * 90;
+setTimeout(typeWriterParagraph, titleDuration + 300);
